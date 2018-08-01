b19f9ccb0f4d6cc56009b947eedd92648c1d85aa
Subhajit Sahu <wolfram77@gmail.com>
2018-08-01 15:22:08 +0530

    uninstall script should remove pattern from local

--- uninstall.js
+++ uninstall.js
@@ -1,10 +1,10 @@
 var cp = require('child_process');
 var os = require('os');
 
-const LBASH = '$HOME/.bashrc';
+const LBASH = '$HOME/.nodef/.bashrc';
 const LTEMP = '$HOME/.nodef/.bashrc-temp';
-const PATTERN = 'source $PWD/index.cmd';
+const LPATTERN = `source ${process.cwd()}/index.cmd`;
 if(os.EOL==='\n') cp.execSync(
-  `grep -v "${PATTERN}" ${LBASH} > ${LTEMP} && `+
-  `cp ${LTEMP} ${LBASH}`
+  `grep -v "${LPATTERN}" ${LBASH} > ${LTEMP} && `+
+  `mv ${LTEMP} ${LBASH}`
 );
