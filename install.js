d3d44ebd001ee08549258101221b6ab7780b6417
Subhajit Sahu <wolfram77@gmail.com>
2018-08-01 14:29:19 +0530

    try delete symlink directly (again)

--- install.js
+++ install.js
@@ -3,6 +3,6 @@ var os = require('os');
 
 if(os.EOL==='\n') cp.execSync(
   `tr -d '\r' <index.sh >index.cmd && `+
-  'chmod +x index.cmd && rm index.sh && rm ../../../bin/ecd '+
+  'chmod +x index.cmd && rm index.sh && rm ../../../bin/ecd && '+
   'echo "source $PWD/index.cmd" >> $HOME/.bashrc'
 );
