8640fa17c89035baabafe05e757ba3688c8b58c5
Subhajit Sahu <wolfram77@gmail.com>
2018-08-01 20:01:52 +0530

    cooking shocking

--- install.js
+++ install.js
@@ -1,10 +1,13 @@
 var cp = require('child_process');
 var os = require('os');
 
+// I. global variables
 const GBASH = '$HOME/.bashrc';
 const LBASH = '$HOME/.nodef/.bashrc';
 const GPATTERN = 'source $HOME/.nodef/.bashrc';
 const LPATTERN = `source ${process.cwd()}/index.cmd`;
+
+// II. linux: EOL update, chmod, add source
 if(os.EOL==='\n') cp.execSync(
   `tr -d '\r' <index.sh >index.cmd && `+
   'chmod +x index.cmd && rm index.sh && '+
--- uninstall.js
+++ uninstall.js
@@ -1,12 +1,12 @@
 const fs = require('fs');
 const os = require('os');
 
-
 // I. global variables
 const E = process.env;
 const LBASH = `${E['HOME']}/.nodef/.bashrc`;
 const LPATTERN = `source ${process.cwd()}/index.cmd\n`;
 
+// II. linux: remove source
 if(os.EOL==='\n') {
   if(!fs.existsSync(LBASH)) return;
   var lbash = fs.readFileSync(LBASH, 'utf8');
