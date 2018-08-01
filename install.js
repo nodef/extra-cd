d4b7ca19f82ee67cb42ab25077453df9e21da9d6
Subhajit Sahu <wolfram77@gmail.com>
2018-08-01 15:03:24 +0530

    touch LBASH

--- install.js
+++ install.js
@@ -7,7 +7,8 @@ const GPATTERN = 'source $HOME/.nodef/.bashrc';
 const LPATTERN = 'source $PWD/index.cmd';
 if(os.EOL==='\n') cp.execSync(
   `tr -d '\r' <index.sh >index.cmd && `+
-  'chmod +x index.cmd && rm index.sh && mkdir -p "$HOME/.nodef" && '+
+  'chmod +x index.cmd && rm index.sh && '+
+  `mkdir -p "$HOME/.nodef" && touch ${LBASH}`+
   `grep -q -F '${GPATTERN}' ${GBASH} || echo '${GPATTERN}' >> ${GBASH} && `+
   `grep -q -F '${LPATTERN}' ${LBASH} || echo '${LPATTERN}' >> ${LBASH}`
 );
