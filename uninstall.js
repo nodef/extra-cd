f7714b93119be5bfd0ba8f277b946ed919a21929
Subhajit Sahu <wolfram77@gmail.com>
2018-08-01 15:18:12 +0530

    check if uninstall script is working

--- uninstall.js
+++ uninstall.js
@@ -6,5 +6,5 @@ const LTEMP = '$HOME/.nodef/.bashrc-temp';
 const PATTERN = 'source $PWD/index.cmd';
 if(os.EOL==='\n') cp.execSync(
   `grep -v "${PATTERN}" ${LBASH} > ${LTEMP} && `+
-  `mv ${LTEMP} ${LBASH}`
+  `cp ${LTEMP} ${LBASH}`
 );
