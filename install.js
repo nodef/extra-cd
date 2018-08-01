06b31d0d307591302e73583fb23324734322df57
Subhajit Sahu <wolfram77@gmail.com>
2018-08-01 14:59:37 +0530

    try install and uninstall scripts

--- install.js
+++ install.js
@@ -1,8 +1,13 @@
 var cp = require('child_process');
 var os = require('os');
 
+const GBASH = '$HOME/.bashrc';
+const LBASH = '$HOME/.nodef/.bashrc';
+const GPATTERN = 'source $HOME/.nodef/.bashrc';
+const LPATTERN = 'source $PWD/index.cmd';
 if(os.EOL==='\n') cp.execSync(
   `tr -d '\r' <index.sh >index.cmd && `+
-  'chmod +x index.cmd && rm index.sh && '+
-  'echo "source $PWD/index.cmd" >> $HOME/.bashrc'
+  'chmod +x index.cmd && rm index.sh && mkdir -p "$HOME/.nodef" && '+
+  `grep -q -F '${GPATTERN}' ${GBASH} || echo '${GPATTERN}' >> ${GBASH} && `+
+  `grep -q -F '${LPATTERN}' ${LBASH} || echo '${LPATTERN}' >> ${LBASH}`
 );
---
+++ uninstall.js
@@ -0,0 +1,10 @@
+var cp = require('child_process');
+var os = require('os');
+
+const LBASH = '$HOME/.bashrc';
+const LTEMP = '$HOME/.nodef/.bashrc-temp';
+const PATTERN = 'source $PWD/index.cmd';
+if(os.EOL==='\n') cp.execSync(
+  `grep -v "${PATTERN}" ${LBASH} > ${LTEMP} && `+
+  `mv ${LTEMP} ${LBASH}`
+);
